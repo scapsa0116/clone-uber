21e6bf585c5522a1e913b1959f21ac384d0783d2
stela <stela.capsa@gmail.com>
2021-06-20 12:35:13 -0500

    uber-clone

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MapView } from 'expo';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = 
+    {
+      region : 
+      {
+        latitude: 1.290270,
+        longitude: 103.851959,
+        latitudeDelta: 0.02,
+        longitudeDelta: 0.02,
+      }
+    }
+  }
+  render()
+  {
+    return (
+      <View style={styles.container}>
+        <Text>HomeScreen!</Text>
+        <MapView 
+        initialRegion = {this.state.region}
+        showsUserLocation = {true}
+        showsCompass = {true}
+        rotateEnabled = {false}
+        style = {{flex:1}}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
